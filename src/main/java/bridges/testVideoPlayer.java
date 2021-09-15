140251a237cd10a333d66ee0b36155dd0252b365
Kunmy <992711628@qq.com>
2021-09-16 04:42:23 +0800

    test_2

---
+++ src/main/java/bridges/testVideoPlayer.java
@@ -0,0 +1,10 @@
+package bridges;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class testVideoPlayer {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
