ab574bf1cc2823ad2ff541c3636ffb638fdc1aff
Kunmy <992711628@qq.com>
2021-09-17 00:49:58 +0800

    大概写了一下README的框架

---
+++ README.md
@@ -0,0 +1,6 @@
+# Bridges
+**A website to watch movies together(co-view website).**
+## Getting Started
+## Installation
+## Contributor
+## License
---
+++ src/main/java/bridges/SBStarter.java
@@ -0,0 +1,11 @@
+package bridges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(SBStarter.class, args);
+    }
+}
